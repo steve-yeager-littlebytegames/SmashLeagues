d05b87d59809c963dfbe9eaeaceca8c88c0a00bd
Unknown <steve.yeager@littlebytegames.com>
2017-01-16 00:44:29 -0800

    Added unit tests

---
+++ Source/SmashLeagues.Droid.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿
+using NUnit.Framework;
+
+namespace SmashLeagues.Droid.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.Pass();
+        }
+    }
+}
