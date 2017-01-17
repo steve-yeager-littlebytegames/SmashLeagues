d5c5899c246c5bf99b223eb74743fcb0d1dc16b4
Unknown <steve.yeager@littlebytegames.com>
2017-01-16 22:13:52 -0800

    Login page layout

--- Source/SmashLeagues/SmashLeagues/MainPage.xaml.cs
+++ Source/SmashLeagues/SmashLeagues/MainPage.xaml.cs
@@ -1,18 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.WindowsAzure.MobileServices;
 using Xamarin.Forms;
 
 namespace SmashLeagues
 {
     public partial class MainPage : ContentPage
     {
+        public const string ApplicationUrl = @"https://smashleagues.azurewebsites.net";
         public static string name = "Steve";
+
+        private MobileServiceClient client;
+
         public MainPage()
         {
             InitializeComponent();
+            client = new MobileServiceClient(ApplicationUrl);
+            
         }
     }
 }
